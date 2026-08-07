ec2e0be2949a7438d22b0539e77001cf80e30a5b
AdityaTripathi21 <atriptrip21@gmail.com>
2026-08-07 18:31:10 -0400

    delete old stuff

--- db.py
+++
@@ -1,9 +0,0 @@
-import chromadb
-
-client = chromadb.PersistentClient(path="data/chroma")
-collection = client.get_or_create_collection(
-    name = "laptop_search",
-    embedding_function=None
-    )
-
-
